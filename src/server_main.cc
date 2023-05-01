88998feb6a3b34d90452d4e13fd5ff6455e387d6
Christy Yeung <cyeung5@g.ucla.edu>
2023-05-01 04:06:39 +0000

    Added trivial console logging to main server file
    Change-Id: I702948b0838e883d43a086c59cc3927d4cacad30

--- src/server_main.cc
+++ src/server_main.cc
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/log/trivial.hpp>
 #include "http_server.h"
 #include "config_parser.h"
 
@@ -13,6 +14,16 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
       std::cerr << "Usage: ./server <path to config file>\n";
+      if (argc < 2)
+      {
+        BOOST_LOG_TRIVIAL(error) << "Too few arguments to start server.";
+        BOOST_LOG_TRIVIAL(info) << "Server not started.";
+      }
+      else
+      {
+        BOOST_LOG_TRIVIAL(error) << "Too many arguments to start server.";
+        BOOST_LOG_TRIVIAL(info) << "Server not started.";
+      }
       return 1;
     }
     NginxConfigParser config_parser;
@@ -23,11 +34,13 @@ int main(int argc, char *argv[])
     }
 
     boost::asio::io_service io_service;
+    BOOST_LOG_TRIVIAL(info) << "Starting the server.";
     HTTPserver s(config, io_service);
   }
   catch (std::exception &e)
   {
     std::cerr << "Exception: " << e.what() << "\n";
+    BOOST_LOG_TRIVIAL(error) << "Exception: " << e.what();
   }
 
   return 0;
